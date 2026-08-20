1629e5fd752dc7906a5818c2836a2364ea0cf1e3
Angelina <angelina.kalga@mail.ru>
2026-08-20 18:37:04 +0400

    update docs

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,14 +7,14 @@ export default {
   routes: [
   {
     "renderMode": 2,
-    "route": "/Landing-Macaroons-Angular"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 672, hash: '0812cbd768158f1125f9a63b2d0a3f43cec07e439f22226270f3708f970e9d53', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1045, hash: '31e67a1c639fb00f6434d4b3d3e5fba930f32ec34105dd62986c9f8d52d524fb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'Landing-Macaroons-Angular/index.html': {size: 37094, hash: '001a9f4e8f616986b7ae5d455b9824c0bbb89d2e20bdd105c79d6498bd8a2c08', text: () => import('./assets-chunks/Landing-Macaroons-Angular_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 646, hash: 'c8ed7028ffb5a662e742f39ccda2435d7664759ae8232fad391623e7bde3ae94', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1019, hash: '9a30a57355d121ee0c79694da6f7f76721bff93292f8b8189622feb88a7a53f6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 38125, hash: 'fee2f68106a780f933289f12e028d1d907bcc65f4ada829e9b96a8cf9a0af272', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-NV5VJXJX.css': {size: 163, hash: 'K64/dqIErsA', text: () => import('./assets-chunks/styles-NV5VJXJX_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -3,12 +3,12 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>Macaroons</title>
-  <base href="/Landing-Macaroons-Angular/">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <style>body{margin:0}</style><link rel="stylesheet" href="styles-NV5VJXJX.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-NV5VJXJX.css"></noscript></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-UQMZ2446.js" type="module"></script><script src="main-KVCYLDQB.js" type="module"></script></body>
+<script src="polyfills-UQMZ2446.js" type="module"></script><script src="main-SR3MJIKS.js" type="module"></script></body>
 </html>
 `;
