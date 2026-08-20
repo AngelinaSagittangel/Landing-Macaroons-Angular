9d85ba416a5902cb212e63a5fe32e2291a650d46
Angelina <angelina.kalga@mail.ru>
2026-08-20 18:59:05 +0400

    update docs

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 646, hash: '453218ee528a8a5ad61ce48a95201a8bae3804695402093d503f8cd75343a45a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1019, hash: '183280fcf560e863c080644e3d359bc86fd9632a69468969617bae543cd680fc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 37781, hash: '4645465bc8f147abc95a8eded38e891a65852921349cf2519b3d4cb87e082d5a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 646, hash: '922d0382713ecc2477e9a65ad937b4c4b400fb10f77105889046d43842d6e1bd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1019, hash: '30474f78cdd0a02d9e66c975d2e87b6b3b174a236bcf02b6641fdded5cb26bfb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 38204, hash: '79efda7fcf817af2ccf2ee5339b17483fa762656dbc10675dfaa3828ec682945', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-NV5VJXJX.css': {size: 163, hash: 'K64/dqIErsA', text: () => import('./assets-chunks/styles-NV5VJXJX_css.mjs').then(m => m.default)}
   },
 };
